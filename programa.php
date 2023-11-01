41919a443b8a5a2d6ead8864c2b2f3f6d4cfe40f
halier <halierandres26@gmail.com>
2023-11-01 07:32:12 -0500

    Actualizacion

---
+++ programa.php
@@ -0,0 +1,38 @@
+<?php
+include 'index.php';
+include 'conexion.php';
+?>
+
+<h1> Listado de facultades </h1>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Codigo</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Opciones</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php
+        $sql = "SELECT * FROM programas";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>".$row["codigo_prg"]."</td>";
+                echo "<td>".$row["nombre_prg"]."</td>";
+                echo "<td>".$row["facultad"]."</ts>";
+                echo "<td>
+                    <a class='btn btn-success'>Editar</a>
+                    <a class='btn btn-danger'>Eliminar</a>
+                    </td>";
+                echo "</tr>";
+            }
+        } else {
+            echo "0 resultados";
+        }
+        $conn->close();
+    ?>
+  </tbody>
+</table>
